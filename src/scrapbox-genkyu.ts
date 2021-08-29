92b18fbfc1227563d97dfe9b54cc9f87d5729b6a
ci7lus <7887955+ci7lus@users.noreply.github.com>
2021-08-29 17:19:52 +0900

    :sparkles: scrapbox-genkyu

---
+++ src/scrapbox-genkyu.ts
@@ -0,0 +1,73 @@
+import { serve } from "https://deno.land/x/sift@0.3.5/mod.ts"
+import * as $ from "https://deno.land/x/zod@v3.8.0/mod.ts"
+import { parseFeed } from "https://deno.land/x/rss@0.5.3/mod.ts"
+import { Feed } from "https://jspm.dev/feed"
+
+const handleRequest = async (_: Request, params: unknown) => {
+  const parsedParams = await $.object({
+    projectId: $.string().min(1),
+    keyword: $.string().min(1),
+  }).safeParseAsync(params)
+  if (parsedParams.success === false) {
+    return new Response(JSON.stringify(parsedParams.error), {
+      status: 400,
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+  }
+  const { projectId, keyword } = parsedParams.data
+  const decodedKeyword = decodeURIComponent(keyword)
+
+  const response = await fetch(`https://scrapbox.io/api/feed/${projectId}`, {
+    headers: {
+      "user-agent": "scrapbox-genkyu",
+    },
+  })
+
+  if (!response.ok) {
+    return new Response("fetch error", {
+      status: 500,
+    })
+  }
+
+  const text = await response.text()
+  const parsed = await parseFeed(text)
+
+  const feed = new Feed({
+    id: parsed.id,
+    title: `${decodedKeyword} in ${parsed.title.value}`,
+    description: parsed.description,
+    link: parsed.links[0],
+    updated: parsed.updateDate,
+    generator: "scrapbox-genkyu",
+  })
+
+  parsed.entries
+    .filter(
+      (entry) =>
+        decodeURIComponent(entry.id).includes(decodedKeyword) ||
+        entry.title?.value?.includes(decodedKeyword) ||
+        entry.description?.value?.includes(decodedKeyword)
+    )
+    .forEach((entry) => {
+      feed.addItem({
+        id: entry.id,
+        title: entry.title?.value,
+        description: entry.description?.value,
+        link: entry.links[0].href,
+        date: entry.published,
+      })
+    })
+
+  return new Response(feed.rss2(), {
+    headers: {
+      "content-type": "application/rss+xml; charset=utf-8",
+    },
+  })
+}
+
+serve({
+  "/": () => new Response("scrapbox-genkyu: /:projectId/:keyword"),
+  "/:projectId/:keyword": handleRequest,
+})
