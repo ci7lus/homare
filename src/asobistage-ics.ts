1ab50fbaebacea0af5d9c4a7217210e93c33a6f2
ci7lus <7887955+ci7lus@users.noreply.github.com>
2022-01-16 16:11:27 +0900

    ✨ [asb] day2以降に対応

--- src/asobistage-ics.ts
+++ src/asobistage-ics.ts
@@ -1,89 +1,125 @@
-import { serve } from "https://deno.land/x/sift@0.4.2/mod.ts"
-import ics from "https://cdn.skypack.dev/ics"
+import { serve } from "https://deno.land/x/sift@0.4.2/mod.ts";
+import ics from "https://cdn.skypack.dev/ics";
+import { DateTime, datetime } from "https://deno.land/x/ptera@v1.0.2/mod.ts";
 
 const SOURCE_URL =
-  "https://github.com/ci7lus/homare/blob/master/src/asobistage-ics.ts"
-const MAX_AGE = 60 * 60 * 12
+  "https://github.com/ci7lus/homare/blob/master/src/asobistage-ics.ts";
+const MAX_AGE = 60 * 60 * 12;
 
-const dateToArr = (date: Date) => [
-  date.getFullYear(),
-  date.getMonth() + 1,
-  date.getDate(),
-  date.getHours(),
-  date.getMinutes(),
-]
+const dateToArr = (d: DateTime) => {
+  const date = d.toDateObj();
+  return [date.year, date.month, date.day, date.hour, date.minute];
+};
 
 const handleRequest = async () => {
   const response = await fetch("https://asobistage.asobistore.jp/", {
     headers: {
       "user-agent": `asobistage-ics (+${SOURCE_URL})`,
     },
-  })
+  });
 
   if (!response.ok) {
     return new Response("fetch error", {
       status: 500,
-    })
+    });
   }
 
-  const text = await response.text()
-  const json = text.match(/type=\"application\/json">(.+)<\/script>/)?.[1]
+  const text = await response.text();
+  const json = text.match(/type=\"application\/json">(.+)<\/script>/)?.[1];
 
   if (!json) {
     return new Response("json not found", {
       status: 500,
-    })
+    });
   }
   const next: {
     props: {
       pageProps: {
         initialData: {
           contents: {
-            id: string
-            createdAt: string
-            updatedAt: string
-            publishedAt: string
-            revisedAt: string
-            type: string[]
-            title: string
-            text_eventdate: string
-            text_archivedate: string
-            listview_date: string
-            countdown_live?: string
-            ticket_link: string
-            official_link: string
-            otherLinks: []
-            hashEventpage: boolean
-            pickup: boolean
-          }[]
-        }
-      }
-    }
-  } = JSON.parse(json)
+            id: string;
+            createdAt: string;
+            updatedAt: string;
+            publishedAt: string;
+            revisedAt: string;
+            type: string[];
+            title: string;
+            text_eventdate: string;
+            text_archivedate: string;
+            listview_date: string;
+            countdown_live?: string;
+            ticket_link: string;
+            official_link: string;
+            otherLinks: [];
+            hashEventpage: boolean;
+            pickup: boolean;
+          }[];
+        };
+      };
+    };
+  } = JSON.parse(json);
 
   const { error, value } = ics.createEvents(
     next.props.pageProps.initialData.contents
       .filter((item) => item.countdown_live)
       .map((item) => {
-        return {
-          uid: item.id,
-          start: dateToArr(new Date(item.countdown_live!)),
-          created: dateToArr(new Date(item.createdAt)),
-          lastModified: dateToArr(new Date(item.revisedAt)),
-          duration: { hours: 1 },
-          title: item.title,
-          url: item.ticket_link?.startsWith("/")
-            ? `https://asobistage.asobistore.jp${item.ticket_link}`
-            : item.ticket_link,
-          productId: "asobistage/ics",
-        }
+        const countdown = datetime(item.countdown_live!, {
+          timezone: "UTC",
+        }).toZonedTime("Asia/Tokyo");
+        const created = datetime(item.createdAt, {
+          timezone: "UTC",
+        }).toZonedTime("Asia/Tokyo");
+        const lastModified = datetime(item.revisedAt, {
+          timezone: "UTC",
+        }).toZonedTime("Asia/Tokyo");
+        return item.listview_date
+          .split(/・|〜|~/)
+          .map((dateStr, idx) => {
+            const dateIdent = dateStr
+              .split(/\.|\//)
+              .map((n) => parseInt(n.trim()));
+            let year: number;
+            let month: number;
+            let day: number;
+            if (3 <= dateIdent.length) {
+              if (3 < dateIdent.length) {
+                console.warn(`Unexpected splitter: ${dateStr}`);
+              }
+              [year, month, day] = dateIdent;
+            } else {
+              year = countdown.year;
+              [month, day] = dateIdent;
+            }
+            const target = datetime(item.countdown_live!, {
+              timezone: "UTC",
+            })
+              .toZonedTime("Asia/Tokyo")
+              .toDateObj();
+            target.year = year;
+            target.month = month;
+            target.day = day;
+            return [datetime(target, { timezone: "Asia/Tokyo" }), idx] as const;
+          })
+          .map(([datetime, idx]) => ({
+            uid: item.id + "-" + idx,
+            start: dateToArr(datetime),
+            created: dateToArr(created),
+            lastModified: dateToArr(lastModified),
+            duration: { hours: 1 },
+            title: `${item.title} (Day ${idx + 1})`,
+            url: item.ticket_link?.startsWith("/")
+              ? `https://asobistage.asobistore.jp${item.ticket_link}`
+              : item.ticket_link,
+            productId: "asobistage/ics",
+          }));
       })
-  )
+      .flat(),
+  );
   if (error) {
-    console.error(error)
+    console.error(error);
     return new Response("ical generation error", {
       status: 500,
-    })
+    });
   }
 
   return new Response(value, {
@@ -91,10 +127,10 @@ const handleRequest = async () => {
       "content-type": "text/calendar; charset=utf-8",
       "cache-control": `max-age=${MAX_AGE}`,
     },
-  })
-}
+  });
+};
 
 serve({
   "/": () => new Response(`asobistage-ics: /calendar.ics (+${SOURCE_URL})`),
   "/calendar.ics": handleRequest,
-})
+});
