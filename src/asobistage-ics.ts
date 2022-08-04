b899f9dfa34fe4afab83c6d32174d50cb94f8e6e
ci7lus <7887955+ci7lus@users.noreply.github.com>
2022-08-04 15:28:26 +0900

    :art: [asobistage-ics] parse event period

--- src/asobistage-ics.ts
+++ src/asobistage-ics.ts
@@ -72,50 +72,94 @@ const handleRequest = async () => {
         const lastModified = datetime(item.revisedAt, {
           timezone: "UTC",
         }).toZonedTime("Asia/Tokyo");
-        return item.listview_date
-          .split(/・|〜|~|-/)
-          .map((dateStr, idx) => {
-            const dateIdent = dateStr
-              .split(/\.|\//)
-              .map((n) => parseInt(n.trim()));
-            let year: number;
-            let month: number;
-            let day: number;
-            if (3 <= dateIdent.length) {
-              if (3 < dateIdent.length) {
-                console.warn(`Unexpected splitter: ${dateStr}`);
-              }
-              [year, month, day] = dateIdent;
-            } else if (2 === dateIdent.length) {
-              year = countdown.year;
-              [month, day] = dateIdent;
-            } else {
-              year = countdown.year;
-              month = countdown.month;
-              [day] = dateIdent;
+
+        const listviewDate = item.listview_date
+          .split(/・|〜|～|~|-|\s\/\s|\n/)
+          .map((d) => d.trim())
+          .filter((s) => !!s);
+        const eventDate = item.text_eventdate
+          .split(/・|〜|～|~|-|\s\/\s|\n/)
+          .map((d) => d.trim())
+          .filter((s) => !!s);
+
+        const parseTextDate = (dateStr: string) => {
+          const dateIdent = dateStr
+            .split(/\.|\s|:|\//)
+            .map((n) => parseInt(n.trim()))
+            .filter((n) => !Number.isNaN(n));
+          let year: number;
+          let month: number;
+          let day: number;
+          let hour: number | null = null;
+          let minute: number | null = null;
+          if (5 <= dateIdent.length) {
+            if (5 < dateIdent.length) {
+              console.warn(`Unexpected splitter: ${dateStr}`);
             }
-            const target = datetime(item.countdown_live!, {
-              timezone: "UTC",
-            })
-              .toZonedTime("Asia/Tokyo")
-              .toDateObj();
-            target.year = year;
-            target.month = month;
-            target.day = day;
-            return [datetime(target, { timezone: "Asia/Tokyo" }), idx] as const;
+            [year, month, day, hour, minute] = dateIdent;
+          } else if (3 <= dateIdent.length) {
+            [year, month, day] = dateIdent;
+          } else if (2 === dateIdent.length) {
+            year = countdown.year;
+            if (dateStr.includes(":")) {
+              month = countdown.month;
+              day = countdown.day;
+              [hour, minute] = dateIdent;
+            } else {
+              [month, day] = dateIdent;
+            }
+          } else {
+            year = countdown.year;
+            month = countdown.month;
+            [day] = dateIdent;
+          }
+          const target = datetime(item.countdown_live!, {
+            timezone: "UTC",
           })
-          .map(([datetime, idx]) => ({
-            uid: item.id + "-" + idx,
-            start: dateToArr(datetime),
-            created: dateToArr(created),
-            lastModified: dateToArr(lastModified),
-            duration: { hours: 1 },
-            title: `${item.title} (Day ${idx + 1})`,
-            url: item.ticket_link?.startsWith("/")
-              ? `https://asobistage.asobistore.jp${item.ticket_link}`
-              : item.ticket_link,
-            productId: "asobistage/ics",
-          }));
+            .toZonedTime("Asia/Tokyo")
+            .toDateObj();
+          target.year = year;
+          target.month = month;
+          target.day = day;
+          if (hour !== null && minute !== null) {
+            target.hour = 0;
+            target.minute = 0;
+          }
+          const dt = datetime(target, { timezone: "Asia/Tokyo" });
+          if (hour !== null && minute !== null) {
+            dt.add({ hour, minute });
+          }
+          return dt;
+        };
+
+        const [lvStart, lvEnd] = listviewDate.map(parseTextDate);
+        const [evStart, evEnd] = eventDate.map(parseTextDate);
+        const createEvent = (datetime: DateTime, idx?: number) => ({
+          uid: item.id + "-" + (idx || 0),
+          start: dateToArr(datetime),
+          created: dateToArr(created),
+          lastModified: dateToArr(lastModified),
+          duration: { hours: 1 },
+          title: `${item.title}${idx !== undefined ? ` (Day ${idx + 1})` : ""}`,
+          url: item.ticket_link?.startsWith("/")
+            ? `https://asobistage.asobistore.jp${item.ticket_link}`
+            : item.ticket_link,
+          productId: "asobistage/ics",
+        });
+        // listviewだと長過ぎる: THE IDOLM@STER MILLION LIVE!7thLIVE Q@MP FLYER!!! Reburn
+        // eventdateだと長過ぎる: オンラインドラマシアター「人間椅子」
+        if (!lvEnd || !evEnd) {
+          return [createEvent(lvStart)];
+        }
+        const diffInDate =
+          (evEnd.toMilliseconds() - evStart.toMilliseconds()) /
+          1000 /
+          60 /
+          60 /
+          24;
+        return [...Array(Math.floor(diffInDate) + 1).keys()]
+          .map((add) => [datetime(evStart).add({ day: add }), add] as const)
+          .map(([datetime, idx]) => createEvent(datetime, idx));
       })
       .flat()
   );
