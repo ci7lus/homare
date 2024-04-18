e34f89dabbe42d162c609e7afdd47f8dbbc62e93
ci7lus <7887955+ci7lus@users.noreply.github.com>
2024-04-18 22:31:45 +0900

    fix(asobistage-ics): fix source

--- src/asobistage-ics.ts
+++ src/asobistage-ics.ts
@@ -1,6 +1,9 @@
 import { serve } from "https://deno.land/x/sift@0.6.0/mod.ts";
 import ics from "https://cdn.skypack.dev/ics@v2.35.0";
 import { DateTime, datetime } from "https://deno.land/x/ptera@v1.0.2/mod.ts";
+import ical from "npm:ical-generator@7.0.0";
+import { getVtimezoneComponent } from "npm:@touch4it/ical-timezones@1.9.0";
+import dayjs from "npm:dayjs@1.11.10";
 
 const SOURCE_URL =
   "https://github.com/ci7lus/homare/blob/master/src/asobistage-ics.ts";
@@ -12,152 +15,71 @@ const dateToArr = (d: DateTime) => {
 };
 
 const handleRequest = async () => {
-  const response = await fetch(
-    "https://asobistage.microcms.io/api/v1/event?limit=6&filters=pickup%5Bequals%5Dtrue",
-    {
-      headers: {
-        "X-Microcms-Api-Key": "ece26e2c-22ab-4e3b-98ed-d6daca970eeb",
-      },
-    }
-  );
+  const [cdnEventsReq, microEventsReq] = await Promise.all([
+    fetch("https://asobistage.asobistore.jp/cdn/v101/commons/event_list.json"),
+    fetch(
+      "https://asobistage.microcms.io/api/v1/event?limit=6&filters=pickup%5Bequals%5Dtrue",
+      {
+        headers: {
+          "X-Microcms-Api-Key": "ece26e2c-22ab-4e3b-98ed-d6daca970eeb",
+        },
+      }
+    ),
+  ]);
 
-  if (!response.ok) {
+  if (!cdnEventsReq.ok || !microEventsReq.ok) {
     return new Response("fetch error", {
       status: 500,
     });
   }
-  const next: {
+
+  const cdnEvents: {
+    events: { slug: string; event_performance_date: string[] }[];
+  } = await cdnEventsReq.json();
+  const microEvents: {
     contents: {
       id: string;
-      createdAt: string;
-      updatedAt: string;
-      publishedAt: string;
-      revisedAt: string;
-      type: string[];
       title: string;
-      text_eventdate: string;
-      text_archivedate: string;
-      listview_date: string;
       countdown_live?: string;
       ticket_link: string;
-      official_link: string;
-      otherLinks: [];
-      hashEventpage: boolean;
-      pickup: boolean;
     }[];
-  } = await response.json();
+  } = await microEventsReq.json();
 
-  const { error, value } = ics.createEvents(
-    next.contents
-      .map((item) => {
-        const countdown = datetime(item.countdown_live!, {
-          timezone: "UTC",
-        });
-        const created = datetime(item.createdAt, {
-          timezone: "UTC",
-        });
-        const lastModified = datetime(item.revisedAt, {
-          timezone: "UTC",
-        });
+  const calendar = ical({ name: "Streaming+" });
+  calendar.timezone({
+    name: "Asia/Tokyo",
+    generator: getVtimezoneComponent,
+  });
 
-        const listviewDate = item.listview_date
-          .split(/・|〜|～|~|-|\s\/\s|\n/)
-          .map((d) => d.trim())
-          .filter((s) => !!s);
-        const eventDate = item.text_eventdate
-          .split(/・|〜|～|~|-|\s\/\s|\n/)
-          .map((d) => d.trim())
-          .filter((s) => !!s);
+  cdnEvents.events.forEach((cEvent) => {
+    const event = microEvents.contents.find((item) => item.id === cEvent.slug);
+    if (!event || !event.countdown_live) {
+      return;
+    }
+    const startAt = dayjs(event.countdown_live);
+    for (const [dateStr, idx] of cEvent.event_performance_date.map(
+      (d, idx) => [d, idx] as const
+    )) {
+      const date = dayjs(dateStr);
+      const startAtInDate = startAt
+        .clone()
+        .set("year", date.year())
+        .set("month", date.month())
+        .set("date", date.date());
+      calendar.createEvent({
+        id: `${event.id}-${idx}`,
+        start: startAtInDate.toDate(),
+        end: startAtInDate.clone().add(1, "hour").toDate(),
+        summary: `${event.title}${
+          cEvent.event_performance_date.length >= 2 ? ` ${idx + 1}日目` : ""
+        }`,
+        description: `https://asobistage.asobistore.jp${event.ticket_link}`,
+        timezone: "Asia/Tokyo",
+      });
+    }
+  });
 
-        const parseTextDate = (dateStr: string) => {
-          const dateIdent = dateStr
-            .split(/\.|\s|:|\//)
-            .map((n) => parseInt(n.trim()))
-            .filter((n) => !Number.isNaN(n));
-          let year: number;
-          let month: number;
-          let day: number;
-          let hour: number | null = null;
-          let minute: number | null = null;
-          if (5 <= dateIdent.length) {
-            if (5 < dateIdent.length) {
-              console.warn(`Unexpected splitter: ${dateStr}`);
-            }
-            [year, month, day, hour, minute] = dateIdent;
-          } else if (3 <= dateIdent.length) {
-            [year, month, day] = dateIdent;
-          } else if (2 === dateIdent.length) {
-            year = countdown.year;
-            if (dateStr.includes(":")) {
-              month = countdown.month;
-              day = countdown.day;
-              [hour, minute] = dateIdent;
-            } else {
-              [month, day] = dateIdent;
-            }
-          } else {
-            year = countdown.year;
-            month = countdown.month;
-            [day] = dateIdent;
-          }
-          const target = datetime(item.countdown_live!, {
-            timezone: "UTC",
-          })
-            .toZonedTime("Asia/Tokyo")
-            .toDateObj();
-          target.year = year;
-          target.month = month;
-          target.day = day;
-          if (hour !== null && minute !== null) {
-            target.hour = 0;
-            target.minute = 0;
-          }
-          const dt = datetime(target, { timezone: "Asia/Tokyo" });
-          if (hour !== null && minute !== null) {
-            dt.add({ hour, minute });
-          }
-          return dt.toUTC();
-        };
-
-        const [, lvEnd] = listviewDate.map(parseTextDate);
-        const [evStart, evEnd] = eventDate.map(parseTextDate);
-        const createEvent = (datetime: DateTime, idx?: number) => ({
-          uid: item.id + "-" + (idx || 0),
-          start: dateToArr(datetime),
-          created: dateToArr(created),
-          lastModified: dateToArr(lastModified),
-          duration: { hours: 1 },
-          title: `${item.title}${idx !== undefined ? ` (Day ${idx + 1})` : ""}`,
-          url: item.ticket_link?.startsWith("/")
-            ? `https://asobistage.asobistore.jp${item.ticket_link}`
-            : item.ticket_link,
-          productId: "asobistage/ics",
-        });
-        // listviewだと長過ぎる: THE IDOLM@STER MILLION LIVE!7thLIVE Q@MP FLYER!!! Reburn
-        // eventdateだと長過ぎる: オンラインドラマシアター「人間椅子」
-        if (!lvEnd || !evEnd) {
-          return [createEvent(countdown)];
-        }
-        const diffInDate =
-          (evEnd.toMilliseconds() - evStart.toMilliseconds()) /
-          1000 /
-          60 /
-          60 /
-          24;
-        return [...Array(Math.floor(diffInDate) + 1).keys()]
-          .map((add) => [datetime(countdown).add({ day: add }), add] as const)
-          .map(([datetime, idx]) => createEvent(datetime, idx));
-      })
-      .flat()
-  );
-  if (error) {
-    console.error(error);
-    return new Response("ical generation error", {
-      status: 500,
-    });
-  }
-
-  return new Response(value, {
+  return new Response(calendar.toString(), {
     headers: {
       "content-type": "text/plain; charset=utf-8",
       "cache-control": `max-age=${MAX_AGE}`,
